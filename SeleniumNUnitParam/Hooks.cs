6da3a1a20b819152ad87422a3389f753998aeff1
visvaroobini <33253608+visvaroobini@users.noreply.github.com>
2020-04-12 08:40:25 +0100

    Set Chrome Browser

--- SeleniumNUnitParam/Hooks.cs
+++ SeleniumNUnitParam/Hooks.cs
@@ -24,7 +24,9 @@ namespace SeleniumNUnitParam
             //Get the value from NUnit-console --params 
             //e.g. nunit3-console.exe --params:Browser=Firefox \SeleniumNUnitParam.dll
             //If nothing specified, test will run in Chrome browser
-            var browserType = TestContext.Parameters.Get("Browser", "Chrome");
+            // var browserType = TestContext.Parameters.Get("Browser", "Chrome");
+            var browserType = "Chrome";
+
             //Parse the browser Type, since its Enum
             _browserType = (BrowerType)Enum.Parse(typeof(BrowerType), browserType);
             //Pass it to browser
